f4e73262e7a047fca482fb6ff59bb9252ab597c0
abhi8585 <abhi.sharma1114@gmail.com>
2022-03-13 12:51:39 +0530

    commenting aws for prod config

--- config.py
+++ config.py
@@ -16,18 +16,18 @@ class Config(object):
     # Limit the posts to show pagination
     POSTS_PER_PAGE = 10
 
-    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default='minioadmin')
-    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='minioadmin')
-    AWS_BUCKET_NAME = config('AWS_BUCKET_NAME')
-    AWS_ENDPOINT_URL = config('AWS_ENDPOINT_URL',default='http://127.0.0.1:9000')
+    # AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default='minioadmin')
+    # AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='minioadmin')
+    # AWS_BUCKET_NAME = config('AWS_BUCKET_NAME')
+    # AWS_ENDPOINT_URL = config('AWS_ENDPOINT_URL',default='http://127.0.0.1:9000')
 
-    # Mail Config
-    MAIL_SERVER = config('MAIL_SERVER', default='smtppro.zoho.com')
-    MAIL_PORT   = config('MAIL_PORT', default=465)
-    MAIL_USE_SSL = config('MAIL_USE_SSL', default=True)
-    MAIL_USERNAME = config('MAIL_USERNAME',default='pratyush.saxena@dateplanner.in')
-    MAIL_PASSWORD = config('MAIL_PASSWORD', default='Pratyush@123')
-    MAIL_DEFAULT_SENDER = config('MAIL_DEFAULT_SENDER', default= os.environ.get('MAIL_DEFAULT_SENDER','admin@dateplanner.in') )
+    # # Mail Config
+    # MAIL_SERVER = config('MAIL_SERVER', default='smtppro.zoho.com')
+    # MAIL_PORT   = config('MAIL_PORT', default=465)
+    # MAIL_USE_SSL = config('MAIL_USE_SSL', default=True)
+    # MAIL_USERNAME = config('MAIL_USERNAME',default='pratyush.saxena@dateplanner.in')
+    # MAIL_PASSWORD = config('MAIL_PASSWORD', default='Pratyush@123')
+    # MAIL_DEFAULT_SENDER = config('MAIL_DEFAULT_SENDER', default= os.environ.get('MAIL_DEFAULT_SENDER','admin@dateplanner.in') )
     # This will create a file in <app> FOLDER
     SQLALCHEMY_DATABASE_URI = 'mysql://root:''@localhost/mts'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -42,22 +42,15 @@ class ProductionConfig(Config):
     REMEMBER_COOKIE_DURATION = 3600
 
     # AWS S3 Bucket Config
-    AWS_ACCESS_KEY_ID = 'minioadmin'
-    AWS_SECRET_ACCESS_KEY = 'minioadmin'
-    AWS_BUCKET_NAME = 'dp-bucket'
-    AWS_ENDPOINT_URL = None
+    # AWS_ACCESS_KEY_ID = 'minioadmin'
+    # AWS_SECRET_ACCESS_KEY = 'minioadmin'
+    # AWS_BUCKET_NAME = 'dp-bucket'
+    # AWS_ENDPOINT_URL = None
 
   
     
     # PostgreSQL database
-    SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
-        config( 'DB_ENGINE'   , default='postgresql'    ),
-        config( 'DB_USERNAME' , default='appseed'       ),
-        config( 'DB_PASS'     , default='pass'          ),
-        config( 'DB_HOST'     , default='localhost'     ),
-        config( 'DB_PORT'     , default=5432            ),
-        config( 'DB_NAME'     , default='appseed-flask' )
-    )
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:''@localhost/mts'
 
 class DebugConfig(Config):
     DEBUG = True
