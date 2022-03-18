9f3f2e673d2126ed4b0f9a421ffa990328c4420d
abhi8585 <abhi.sharma1114@gmail.com>
2022-03-18 23:42:45 +0530

    registering depo

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def register_extensions(app):
     login_manager.init_app(app)
 
 def register_blueprints(app):
-    for module_name in ('base', 'home', 'master','Users','web','audit','transporter'):
+    for module_name in ('base', 'home', 'master','Users','web','audit','transporter','depo'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
